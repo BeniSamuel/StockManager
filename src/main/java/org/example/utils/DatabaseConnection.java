9a41d9b6b3dbb8bf28304a4f10b2f2294d93e339
benisamuel <benisamuel566@gmail.com>
2025-03-04 12:55:55 +0200

    Established DbConnection

---
+++ src/main/java/org/example/utils/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package org.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private String Dburl = "jdbc:postgres://localhost:5432/products";
+    private String username = "postgres";
+    private String password = "beni@ish";
+    public Connection conn;
+
+    public Connection getConnection () {
+        try {
+          conn = DriverManager.getConnection(Dburl, username, password);
+        }
+        catch ( SQLException e ) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+}
